822c3259c5f15fc739dbd5fd5a76ce4c58c4eb9f
Praneeth Kumar <Praneethrayala135@gmail.com>
2024-10-03 19:34:51 +0530

    Node Module and bable

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Basic React-App Using the Azure Please Work This Time.....
+          Basic React-App Using the Azure Please Work This Time Pakka.....
         </p>
         <a
           className="App-link"
