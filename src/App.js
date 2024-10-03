06a9290a9c2ae6ff83ef696d50641b683f600e79
Praneeth Kumar <Praneethrayala135@gmail.com>
2024-10-03 19:02:03 +0530

    Node Module and builts

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Basic React-App Using the Azure Please Work This Time.....
         </p>
         <a
           className="App-link"
